5cee4341080d871e02aeec5560a3f5a29492974a
mdatikulislam41 <mdatikulislam41@gmail.com>
2025-04-14 13:08:17 +0600

    added dark mode

--- main.js
+++ main.js
@@ -1,8 +1,20 @@
-var details = {
-  name: "John",
-  age: 25,
-  city: "New York",
+var mode = document.getElementById("mode");
+window.onload = () => {
+  const savedTheme = localStorage.getItem("theme");
+  if (savedTheme == "dark") {
+    mode.setAttribute("checked", "checked");
+  }
+  if (savedTheme) {
+    document.documentElement.setAttribute("data-theme", savedTheme);
+  }
 };
-var result = Object.values(details);
-console.log(typeof result);
-console.log(result);
+
+mode.addEventListener("change", function () {
+  if (this.checked) {
+    localStorage.setItem("theme", "dark");
+    document.documentElement.setAttribute("data-theme", "dark");
+  } else {
+    localStorage.setItem("theme", "light");
+    document.documentElement.setAttribute("data-theme", "light");
+  }
+});
