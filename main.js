0b3ffd9188e222471f05e568b131d4799c2d5e97
mdatikulislam41 <mdatikulislam41@gmail.com>
2025-04-14 13:26:12 +0600

    fix dark mode

--- main.js
+++ main.js
@@ -1,20 +1,13 @@
-var mode = document.getElementById("mode");
+const modeToggle = document.getElementById("mode");
+
 window.onload = () => {
-  const savedTheme = localStorage.getItem("theme");
-  if (savedTheme == "dark") {
-    mode.setAttribute("checked", "checked");
-  }
-  if (savedTheme) {
-    document.documentElement.setAttribute("data-theme", savedTheme);
-  }
+  const savedTheme = localStorage.getItem("theme") || "dark"; // Default to light if no theme is saved
+  document.documentElement.setAttribute("data-theme", savedTheme);
+  modeToggle.checked = savedTheme === "dark";
 };
 
-mode.addEventListener("change", function () {
-  if (this.checked) {
-    localStorage.setItem("theme", "dark");
-    document.documentElement.setAttribute("data-theme", "dark");
-  } else {
-    localStorage.setItem("theme", "light");
-    document.documentElement.setAttribute("data-theme", "light");
-  }
+modeToggle.addEventListener("change", () => {
+  const newTheme = modeToggle.checked ? "dark" : "light";
+  localStorage.setItem("theme", newTheme);
+  document.documentElement.setAttribute("data-theme", newTheme);
 });
